8d49f47d59d3070d81156cbd20e558a80d22f53e
Ediannys González <ediannys_16@hotmail.com>
2022-11-25 21:00:44 -0400

    added: selenium for execute javascript script

--- search.py
+++ search.py
@@ -1,6 +1,27 @@
-import webbrowser
-new=2
-list = ["Venezuela", "Canadá", "Brasil", "Colombia"]
-tabUrl="https://www.google.com/search?q="
+from selenium import webdriver 
+import time
+list = ["Tesla Motors", "Canadá", "Brasil", "Colombia"]
 term=list[0]
-webbrowser.open(tabUrl+term,new=new)
+# Use Chrome Explorer to simulate
+driver = webdriver.Chrome() 
+tabUrl="https://www.google.com/search?q="
+# Get the sample website to be scrolled
+driver.get(tabUrl+term) 
+
+# Execute the scroll order by webdriver
+screen_height=driver.execute_script("return window.screen.height;") 
+
+i = 1
+# Record the starting time of the loop
+start = time.time()
+while True:
+    # Scroll 1 screen height each time
+    driver.execute_script("window.scrollTo(0, {screen_height}*{i});".format(screen_height=screen_height, i=i))  
+    i += 0.1
+    # Allow for pause time to load data
+    time.sleep(1)
+    # Record ending time of the whole loop
+    end = time.time()
+    # Break the loop with a given time limit
+    if round(end - start) > 30:
+        break
